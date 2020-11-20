3ebffa5e69012c991a5435e2906a49325311cb07
Sathi <sathi.satb@gmail.com>
2020-11-20 19:57:53 +0530

    Update ForJavafunction.kt

--- ForJavafunction.kt
+++ ForJavafunction.kt
@@ -1,4 +1,4 @@
-/** For knowing Interopearbility Function of Kotlin**/
+/** For knowing Interopearbility Function of Kotlin, refer to JavaFunction.java file**/
 fun main(args:Array<String>) {
     var area = JavaFunction.getArea(8,42) // Function called from Kotlin into our Java File
     println("The area of the rectangle printed from Java File : $area")
@@ -7,3 +7,4 @@ fun main(args:Array<String>) {
 fun add(a:Int,b:Int):Int{ // Function called from Java File into Kotlin
     return a+b
 }
+// Java File does not have Default values option, so to call default value from Kotlin to your Kotlin file, add @JvmOverload just above your function which you want to call
